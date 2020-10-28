d00d77c04f6f1af1f5a378013a0d4adaf06f3fd7
soham4 <68500037+soham4@users.noreply.github.com>
2020-10-28 18:49:29 +0530

    update 2

--- sketch.js
+++ sketch.js
@@ -1,68 +1,51 @@
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-    x = width/2;
-    y = height/2;
-    move1=createButton('Shift Left');
-    move1.position(width/12, y+50);
-    move1.mousePressed(shiftLeft);
+  cnv =createCanvas(windowWidth, windowHeight);
+    background(255);
     
-    move2=createButton('Shift Right');
-    move2.position(width/12, y+70);
-    move2.mousePressed(shiftRight);
     
-    move3=createButton('Shift Up');
-    move3.position(width/12, y+90);
-    move3.mousePressed(shiftUp);
-    
-    move4=createButton('Shift Down');
-    move4.position(width/12, y+110);
-    move4.mousePressed(shiftDown);
-
-    button1=createButton('draw circle');
-    button1.position(width/12, height/2);
-    
-    button2=createButton('draw square');
-    button2.position(width/12, button1.y+30);
-    background('grey');
+    radio = createRadio();
+    radio.option('draw flower');
+    radio.option('draw circle');
+    radio.option('draw square');
+    radio.option('draw triangle');
+    radio.style('width', '110px');
+    radio.position(width/12, height/2);
     
     inp1 = createColorPicker('#ff0000');
-    inp1.position(width/12, button1.y-100);
-    text('select Color', width/12, button1.y-110);
-    
-    slider1 = createSlider(0, 300, 100);
-    slider1.position(width/12, button1.y-40);
-    slider1.style('width', '80px');
-    text('select size', width/12, button1.y-50);
+    inp1.position(width/12, radio.y-100);
+    text('select Color', width/12, radio.y-110);
+    slider1 = createSlider(0, width/5, width/2.5);
+    slider1.position(width/12, radio.y-40);
+    slider1.style('width', 'width/12');
+    text('select size', width/12, radio.y-50);
     
     sel = createSelect();
-    sel.position(width/12, button1.y-150);
+    sel.position(width/12, radio.y-150);
     sel.option('Solid Color');
     sel.option('Outline only');
     
-    fill(255);
-    rectMode(CENTER);
-    rect(width/2, height/2, width/2, height);
-    
-    button1.mousePressed(drawCircle);
-    button2.mousePressed(drawSquare);
 }
 
-/*function draw(){
-    fill(255);
-    rectMode(CENTER);
-    rect(width/2, height/2, width/2, height);
-    
-    drawPointer();
-    
-}*/
+ function draw(){
+     fill(247, 128, 84);
+    rectMode(CORNER);
+    noStroke();
+    rect(0, 0, width/4, height);
+     if(radio.value()==='draw flower'){
+    cnv.mouseClicked(drawFlower);
+  }
+  if(radio.value()==='draw circle'){
+    cnv.mouseClicked(drawCircle);
+  }
+  if(radio.value()==='draw square'){
+    cnv.mouseClicked(drawSquare);
+  }
+  if(radio.value()==='draw triangle'){
+    cnv.mouseClicked(drawTriangle);
+  }
+ }
 
-function drawPointer(){
-    push();
-    fill('yellow');
-    circle(x, y, 10);
-    pop();
-}
  function drawCircle(){
      if(sel.value()==='Solid Color'){
      noStroke;
@@ -72,7 +55,11 @@ function drawPointer(){
          noFill();
          stroke(inp1.color());
      }
-     circle(x, y, slider1.value());
+     if(mouseX>= width/4){
+        a=mouseX;
+        b=mouseY;
+     }
+     circle(a, b, slider1.value());
  }
 function drawSquare(){
     if(sel.value()==='Solid Color'){
@@ -83,17 +70,100 @@ function drawSquare(){
          noFill();
          stroke(inp1.color());
      }
-     rect(width/2, height/2, slider1.value());
+    if(mouseX>= width/4){
+        a=mouseX;
+        b=mouseY;
+     }
+     rect(a, b, slider1.value());
  }
-function shiftLeft(){
-    x= x-10;
+
+function drawTriangle(){
+    if(sel.value()==='Solid Color'){
+     noStroke();
+     fill(inp1.color());
+     }
+     else{
+         noFill();
+         stroke(inp1.color());
+     }
+     if(mouseX>= width/4){
+        a=mouseX;
+        b=mouseY;
+     }
+    //triangle(a, b, a+20, b+30, a-20, b+30);
+    triangle(a, b-slider1.value(), a-(slider1.value()*(sqrt(3)/2)), b+(slider1.value()/2), a+(slider1.value()*(sqrt(3)/2)), b+(slider1.value()/2));
 }
-function shiftRight(){
-    x= x+10;
+
+function drawFlower(){
+     if(mouseX>= width/4){
+        a=mouseX;
+        b=mouseY;
+     }
+    if(sel.value()==='Solid Color'){
+     noStroke();
+     fill(inp1.color());
+     }
+     else{
+         noFill();
+         stroke(inp1.color());
+     }
+        ellipseMode(CORNERS);
+        
+        push();
+        translate(a, b);
+        ellipse(0, 0, slider1.value()*0.5, slider1.value()*0.25);
+        pop();
+        
+        push();
+        translate(a, b);
+        rotate(radians(180));
+        ellipse(0, 0, slider1.value()*0.5, slider1.value()*0.25);
+        pop();
+        
+        
+        push();
+        translate(a, b);
+        rotate(radians(60));
+        ellipse(0, 0, slider1.value()*0.5, slider1.value()*0.25);
+        pop();
+        
+        push();
+        translate(a, b);
+        rotate(radians(300));
+        ellipse(0, 0, slider1.value()*0.5, slider1.value()*0.25);
+        pop();
+  
+
+        push();
+        translate(a, b);
+        rotate(radians(240));
+        ellipse(0, 0, slider1.value()*0.5, slider1.value()*0.25);
+        pop();
+  
+        push();
+        translate(a, b);
+        rotate(radians(120));
+        ellipse(0, 0, slider1.value()*0.5, slider1.value()*0.25);
+        pop();
+  
+        ellipseMode(CENTER);
+        fill('yellow');
+        push();
+        translate(a, b);
+        ellipse(0, 0, slider1.value()*0.25);
+        pop();
+        
 }
-function shiftUp(){
-    y= y-10;
-}
-function shiftDown(){
-    y= y+10;
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+    fill(247, 128, 84);
+    rectMode(CORNER);
+    rect(0, 0, width/4, height);
+    radio.position(width/12, height/2);
+    text('select Color', width/12, radio.y-110);
+    inp1.position(width/12, radio.y-100);
+    text('select size', width/12, radio.y-50);
+    slider1.position(width/12, radio.y-40);
+    slider1.style('width', '100px');
+    sel.position(width/12, radio.y-150);
 }
